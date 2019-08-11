63e139d59c5ac514d75c82b930a33bd71e0aec75
xinoip <xinoipionix@gmail.com>
2019-08-11 12:14:11 +0300

    Now miner can push rocks, pick-up diamonds. After collecting all diamonds, door to the next level will generate randomly on one of the empty tiles.

--- Game/objects/miner.c
+++ Game/objects/miner.c
@@ -16,37 +16,120 @@ void moveMiner(miner_t *miner, level_t *level, SDL_Event event) {
     switch (event.key.keysym.sym) {
         case SDLK_UP:
             if(isValidCellToMove(*level, miner->row-1, miner->col)) {
-                level->map[miner->row][miner->col] = 'E';
-                level->map[miner->row-1][miner->col] = 'P';
-                miner->row--;    
+                // level->map[miner->row][miner->col] = 'E';
+                // level->map[miner->row-1][miner->col] = 'P';
+                // miner->row--;
+                processMove(level, miner, miner->row-1, miner->col, upDir);    
             }
             
             break;
         case SDLK_DOWN:
             if(isValidCellToMove(*level, miner->row+1, miner->col)) {
-                level->map[miner->row][miner->col] = 'E';
-                level->map[miner->row+1][miner->col] = 'P';
-                miner->row++;    
+                // level->map[miner->row][miner->col] = 'E';
+                // level->map[miner->row+1][miner->col] = 'P';
+                // miner->row++;
+                processMove(level, miner, miner->row+1, miner->col, downDir);    
             }
             
             break;
         case SDLK_LEFT:
             if(isValidCellToMove(*level, miner->row, miner->col-1)) {
-                level->map[miner->row][miner->col] = 'E';
-                level->map[miner->row][miner->col-1] = 'P';
-                miner->col--;    
+                // level->map[miner->row][miner->col] = 'E';
+                // level->map[miner->row][miner->col-1] = 'P';
+                // miner->col--;
+                processMove(level, miner, miner->row, miner->col-1, leftDir);    
             }
             
             break;
         case SDLK_RIGHT:
             if(isValidCellToMove(*level, miner->row, miner->col+1)) {
-                level->map[miner->row][miner->col] = 'E';
-                level->map[miner->row][miner->col+1] = 'P';
-                miner->col++;    
+                // level->map[miner->row][miner->col] = 'E';
+                // level->map[miner->row][miner->col+1] = 'P';
+                // miner->col++;
+                processMove(level, miner, miner->row, miner->col+1, rightDir);    
             }
             
             break;
         default:
             break;
     }
+}
+
+void processMove(level_t *level, miner_t *miner, int newRow, int newCol, moveDirection_t dir) {
+
+    switch(level->map[newRow][newCol]) {
+        case 'E':
+            level->map[newRow][newCol] = 'P';   //move miner to new cell
+            level->map[miner->row][miner->col] = 'E';    //clear old cell
+            miner->row = newRow;
+            miner->col = newCol;
+            break;
+        case 'D':
+            level->map[newRow][newCol] = 'P';   //move miner to new cell
+            level->map[miner->row][miner->col] = 'E';    //clear old cell
+            miner->row = newRow;
+            miner->col = newCol;
+            break;
+        case 'X':
+            level->map[newRow][newCol] = 'P';   //move miner to new cell
+            level->map[miner->row][miner->col] = 'E';    //clear old cell
+            miner->row = newRow;
+            miner->col = newCol;
+
+            level->diamondCount--;
+
+            if(level->diamondCount <= 0) {
+                passLevel(level);
+            }
+            break;
+        case 'R':
+            switch (dir) {
+                case leftDir:
+                    if(level->map[newRow][newCol-1] == 'E') {   //rock moveable
+                        level->map[newRow][newCol-1] = 'R';     //move rock
+                        level->map[newRow][newCol] = 'P';       //move miner to rocks old place
+                        level->map[miner->row][miner->col] = 'E';    //clear old miner cell
+                        miner->row = newRow;
+                        miner->col = newCol;
+                    }
+                    break;
+                case rightDir:
+                    if(level->map[newRow][newCol+1] == 'E') {   //rock moveable
+                        level->map[newRow][newCol+1] = 'R';     //move rock
+                        level->map[newRow][newCol] = 'P';       //move miner to rocks old place
+                        level->map[miner->row][miner->col] = 'E';    //clear old miner cell
+                        miner->row = newRow;
+                        miner->col = newCol;
+                    }
+                    break;
+                default:
+                    break;
+                }
+            break;
+        case 'Z':   // DOOR LEVEL CHANGE
+            fillLevel(level, "./assets/maps/mapX.txt");
+            updateMiner(miner, level->startMinerRow, level->startMinerCol);
+            break;
+        default:
+            break;
+    }
+
+}
+
+void passLevel(level_t *level) {
+    
+    int randomRow, randomCol;
+
+    do {
+        randomRow = rand() % level->row;
+        randomCol = rand() % level->col;
+        if(randomRow < level->row && randomCol < level->col) {
+            if(level->map[randomRow][randomCol] == 'E') {
+                break;
+            }
+        }
+    } while(1);
+
+    level->map[randomRow][randomCol] = 'Z';
+
 }
--- Game/objects/miner.h
+++ Game/objects/miner.h
@@ -2,6 +2,7 @@
 #define MINER_H
 
 #include <SDL2/SDL.h>
+#include <stdlib.h>
 #include "./level.h"
 #include "./controls.h"
 
@@ -9,10 +10,17 @@ typedef struct Miner {
     int row, col;
 } miner_t;
 
+typedef enum MoveDirection {
+    upDir, downDir, leftDir, rightDir
+} moveDirection_t;
+
 miner_t createMiner(int row, int col);
 
 void updateMiner(miner_t *miner, int newRow, int newCol);
 
 void moveMiner(miner_t *miner, level_t *level, SDL_Event event);
+
+//pass direction as parameter too for rock moving
+void processMove(level_t *level, miner_t *miner, int newRow, int newCol, moveDirection_t dir);
 
 #endif
