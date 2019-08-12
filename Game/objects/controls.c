f307bcd740edcb653700ecbf79fed021a5fda318
xinoip <xinoipionix@gmail.com>
2019-08-12 16:25:37 +0300

    Created a general function for fall-able objects. Diamonds and Rocks are functional right now.

--- Game/objects/controls.c
+++ Game/objects/controls.c
@@ -9,31 +9,34 @@ bool isValidCellToMove(level_t level, int row, int col) {
 }
 
 void updateMap(level_t *level) {
-    updateRocks(level);
+    //updateRocks(level);
+    updateFallingObjects(level);
 }
 
-void updateRocks(level_t *level) {
+void updateFallingObjects(level_t *level) {
+    char fallingSymbol;
     for(int row = 0; row < level->row; row++) {
         for(int col = 0; col < level->col; col++) {
-            if(level->map[row][col] == 'R') {
-                if(level->map[row+1][col] == 'E') {
+            if(level->map[row][col] == 'R' || level->map[row][col] == 'X') { //if fallable object
+                if(level->map[row][col] == 'R') fallingSymbol = 'B';
+                if(level->map[row][col] == 'X') fallingSymbol = 'T';
+
+                if(level->map[row+1][col] == 'E') { //free fall
                     level->map[row][col] = 'E';
-                    level->map[row+1][col] = 'B';
+                    level->map[row+1][col] = fallingSymbol;
                     
-
-                } else if(level->map[row+1][col] == 'R') {
-                    if(level->map[row+1][col+1] == 'E') {
+                } else if(level->map[row+1][col] == 'R' || level->map[row+1][col] == 'X') { //crash into another fallable object
+                    if(level->map[row][col+1] == 'E' && level->map[row+1][col+1] == 'E') {
                         level->map[row][col] = 'E';
-                        level->map[row+1][col+1] = 'B';
+                        level->map[row+1][col+1] = fallingSymbol;
 
-                    } else if(level->map[row+1][col-1] == 'E') {
+                    } else if(level->map[row][col-1] == 'E' && level->map[row+1][col-1] == 'E') {
                         level->map[row][col] = 'E';
-                        level->map[row+1][col-1] = 'B';
+                        level->map[row+1][col-1] = fallingSymbol;
 
                     }
 
                 }
-
 
             }
 
@@ -43,8 +46,12 @@ void updateRocks(level_t *level) {
 
     for(int row = 0; row < level->row; row++) {
         for(int col = 0; col < level->col; col++) {
-            if(level->map[row][col] == 'B') {
+            if(level->map[row][col] == 'B') { //rocks
                 level->map[row][col] = 'R';
+
+            }
+            if(level->map[row][col] == 'T') { //diamonds
+                level->map[row][col] = 'X';
 
             }
 
