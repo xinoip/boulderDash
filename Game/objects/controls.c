8418100ba560a4edf1dd714e1d066d506cbfe945
xinoip <xinoipionix@gmail.com>
2019-08-14 14:58:31 +0300

    Now miner can die to falling rocks and spiders

--- Game/objects/controls.c
+++ Game/objects/controls.c
@@ -8,41 +8,42 @@ bool isValidCellToMove(level_t level, int row, int col) {
 
 }
 
-void updateMap(level_t *level) {
-    //updateRocks(level);
-    updateFallingObjects(level);
-    updateSpiders(level);
+void updateMap(level_t *level, miner_t *miner) {
+    updateFallingObjects(level, miner);
+    updateSpiders(level, miner);
 }
 
-void updateFallingObjects(level_t *level) {
+void updateFallingObjects(level_t *level, miner_t *miner) {
     char fallingSymbol;
     for(int row = 0; row < level->row; row++) {
         for(int col = 0; col < level->col; col++) {
-            if(level->map[row][col] == 'R' || level->map[row][col] == 'X') { //if fallable object
+            if(level->map[row][col] == 'R' || level->map[row][col] == 'X') { // If fall-able
+
+                // Determine falling symbol
                 if(level->map[row][col] == 'R') fallingSymbol = 'B';
                 if(level->map[row][col] == 'X') fallingSymbol = 'T';
 
-                if(level->map[row+1][col] == 'E') { //free fall
+                if(level->map[row+1][col] == 'E') { // Free fall
                     level->map[row][col] = 'E';
                     level->map[row+1][col] = fallingSymbol;
                     
-                } else if(level->map[row+1][col] == 'R' || level->map[row+1][col] == 'X') { //crash into another fallable object
-                    if(level->map[row][col+1] == 'E' && level->map[row+1][col+1] == 'E') {
+                } else if(level->map[row+1][col] == 'R' || level->map[row+1][col] == 'X') { // Crash into another fallable object
+                    if(level->map[row][col+1] == 'E' && level->map[row+1][col+1] == 'E') {  // Fall to right
                         level->map[row][col] = 'E';
                         level->map[row+1][col+1] = fallingSymbol;
 
-                    } else if(level->map[row][col-1] == 'E' && level->map[row+1][col-1] == 'E') {
+                    } else if(level->map[row][col-1] == 'E' && level->map[row+1][col-1] == 'E') { // Fall to left
                         level->map[row][col] = 'E';
                         level->map[row+1][col-1] = fallingSymbol;
 
                     }
 
-                } else if(level->map[row][col] == 'R' && level->map[row+1][col] == 'S') { //crashing spiderrssss
+                } else if(level->map[row][col] == 'R' && level->map[row+1][col] == 'S') { // Crashing spiders
                     level->map[row][col] = 'E';
                     level->map[row+1][col] = 'B';
                     generateDiaOnDeath(level, row+1, col);
 
-                }
+                } 
 
             }
 
@@ -50,13 +51,24 @@ void updateFallingObjects(level_t *level) {
 
     }
 
+    // Convert falling objects to stationary objects
     for(int row = 0; row < level->row; row++) {
         for(int col = 0; col < level->col; col++) {
-            if(level->map[row][col] == 'B') { //rocks
-                level->map[row][col] = 'R';
+            if(level->map[row][col] == 'B') { // Falling rocks
+                if(level->map[row+1][col] == 'P') { // Crashin player
+                    level->map[row][col] = 'E';
+                    level->map[row+1][col] = 'R';
+                    killMiner(level, miner);
+
+                } else { // Just convert to stationary
+                    level->map[row][col] = 'R';
+
+                }
+                
 
             }
-            if(level->map[row][col] == 'T') { //diamonds
+
+            if(level->map[row][col] == 'T') { // Falling diamonds
                 level->map[row][col] = 'X';
 
             }
@@ -66,47 +78,70 @@ void updateFallingObjects(level_t *level) {
     }
 }
 
-void updateSpiders(level_t *level) {
+void updateSpiders(level_t *level, miner_t *miner) {
 
     for(int row = 0; row < level->row; row++) {
         for(int col = 0; col < level->col; col++) {
 
             if(level->map[row][col] == 'S') {
-                if(level->map[row][col+1] == 'E') { //sag bos ise
-                    //printf("saga gitti\n");
+
+                if(level->map[row][col+1] == 'P') { // Kill player right
                     level->map[row][col] = 'E';
                     level->map[row][col+1] = 'M';
+                    killMiner(level, miner);
 
-                } else if(level->map[row+1][col] == 'E') { //asagisi bos ise
-                    //printf("asagi gitti\n");
+                } else if(level->map[row+1][col] == 'P') { // Kill player bottom
                     level->map[row][col] = 'E';
                     level->map[row+1][col] = 'M';
+                    killMiner(level, miner); 
 
-
-                } else if(level->map[row][col-1] == 'E') { //sol bos ise
-                    //printf("sola gitti\n");
+                } else if(level->map[row][col-1] == 'P') { // Kill player left
                     level->map[row][col] = 'E';
                     level->map[row][col-1] = 'M';
+                    killMiner(level, miner);
 
-                } else if(level->map[row-1][col] == 'E') { //yukari bos ise
-                    //printf("yukari gitti\n");
+                } else if(level->map[row-1][col] == 'P') { // Kill player up
                     level->map[row][col] = 'E';
                     level->map[row-1][col] = 'M';
+                    killMiner(level, miner);
 
                 } else {
-                    printf("ERROR!\n");
+                    // No player in range
+
+                    if(level->map[row][col+1] == 'E') { // Right
+                        level->map[row][col] = 'E';
+                        level->map[row][col+1] = 'M';
+
+                    } else if(level->map[row+1][col] == 'E') { // Bottom
+                        level->map[row][col] = 'E';
+                        level->map[row+1][col] = 'M';
+
+
+                    } else if(level->map[row][col-1] == 'E') { // Left
+                        level->map[row][col] = 'E';
+                        level->map[row][col-1] = 'M';
+
+                    } else if(level->map[row-1][col] == 'E') { // Up
+                        level->map[row][col] = 'E';
+                        level->map[row-1][col] = 'M';
+
+                    } else {
+                        // Spider stuck
+
+                    }
 
                 }
-
+                
             }
 
         }
 
     }
 
+    // Convert moving spiders to stationary spiders
     for(int row = 0; row < level->row; row++) {
         for(int col = 0; col < level->col; col++) {
-            if(level->map[row][col] == 'M') { //moving spiders
+            if(level->map[row][col] == 'M') { // Moving spiders
                 level->map[row][col] = 'S';
 
             }
@@ -146,4 +181,11 @@ void generateDiaOnDeath(level_t *level, int row, int col) {
         level->map[row+1][col-1] = 'X';
     }
 
+}
+
+void killMiner(level_t *level, miner_t *miner) {
+    printf("YOU DIED\n");
+    miner->row = level->startMinerRow;
+    miner->col = level->startMinerCol;
+    level->map[miner->row][miner->col] = 'P';
 }
--- Game/objects/controls.h
+++ Game/objects/controls.h
@@ -1,14 +1,15 @@
 #ifndef CONTROLS_H
 #define CONTROLS_H
 
+typedef struct Miner miner_t;
+
 #include <SDL2/SDL.h>
 #include "../base.h"
 #include "./level.h"
+#include "./miner.h"
 
 bool isValidCellToMove(level_t level, int row, int col);
 
-void updateRocks(level_t *level);
-
-void updateMap(level_t *level);
+void updateMap(level_t *level, miner_t *miner);
 
 #endif
